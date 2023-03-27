1366a55463d7a14d62c7b2e7c6b96ad244a7c014
muaviafbcheckbot <diay5496@gmail.com>
2023-03-27 20:17:19 +0700

    up

--- config-server/config-server.js
+++ config-server/config-server.js
@@ -1,2 +1,2 @@
-const server = "https://a7f1-2001-ee0-41a1-c3b4-8dbf-e6d9-cbd-8f99.ap.ngrok.io";
+const server = "https://9ec9-2001-ee0-41a1-c3b4-8dbf-e6d9-cbd-8f99.ap.ngrok.io";
 // const server = "http://localhost";
